c584b0bea61635530042b36f40b3774af33234d3
stuseks <stuseks@miamioh.edu>
2026-03-16 10:48:18 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// JS Interaction #1
+function showWelcome() {
+    alert("Welcome to Sydney Stusek Photography!");
+}
+
+// JS Interaction #2 (Contact Page)
+function sendMessage() {
+    alert("Thank you! I will get back to you within 24 hours.");
+}
